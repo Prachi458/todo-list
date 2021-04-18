bb23a6207373d7e17849f86a52bcbcecde2b5c27
Prachi458 <pkhushal458@gmail.com>
2021-04-18 10:23:25 -0700

    add and delete record

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+import ToDoForm from './todo-form.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      data : [
+        {
+          title : 'React',
+          description : 'React is a javacsript library',
+        }
+      ],
+      title : '',
+      description : '',
+      editItem : false,
+    };
+  }
+  handleTitle = (event) => {
+    this.setState({ title : event.target.value});
+  };
+  handleDescription = (event) => {
+    this.setState({ description : event.target.value});
+  };
+  addRecord = (event) => {
+    event.preventDefault();
+    const updatedData = [...this.state.data, {title : this.state.title, description : this.state.description}];
+    this.setState({
+      data : updatedData,
+      editItem : false
+    }); 
+    event.target.reset();
+   };
+   handleDelete = (e) => {
+     const updatedData = this.state.data.filter((item,index) => index !== e);
+     this.setState({data : updatedData});
+   };
+   handleEdit = (e) => {
+    const updatedData = this.state.data.filter((item,index) => index !== e);
+    const selectedItem = this.state.data.find((item,index) => index === e);
+    console.log(selectedItem);
+    this.setState({
+      data : updatedData,
+      editItem : true,
+      title : selectedItem,
+      //description
+    });
+   };
+  render(){
+    return(
+      <div>
+      <div>
+      <ToDoForm title={this.state.data} description={this.state.data} editItem={this.state.editItem}
+        handleTitle ={this.handleTitle} handleDescription={this.handleDescription} addRecord={this.addRecord}/>
+         {this.state.data.map((item,index) => {
+        return(
+          <div className="list-btn">
+          <ul className="list"><li>{item.title} &nbsp; : &nbsp; {item.description}</li></ul>
+          <span><button  id="edit-button"  onClick={ () => this.handleEdit(index)}>edit</button>
+          <button  id="delete-button"  onClick={ () => this.handleDelete(index)}>delete</button></span>
+          </div>
+          );
+        })
+        }
+     
 
+      </div>
+      </div>
+    );
+  }
+};
+ReactDOM.render(<App/>,document.getElementById("root"));
 export default App;
---
+++ src/todo-form.js
@@ -0,0 +1,20 @@
+import React from 'react';
+class ToDoForm extends React.Component{
+    render(){
+        return(
+            <div className="App">
+                <form onSubmit={this.props.addRecord}>
+                    <label for="Title" className="input-label">Title</label><br/>
+                    <input className="inputclass" type="text" name="title" value={this.props.data} 
+                    onChange={this.props.handleTitle} required/><br/>
+                    <label for="Description" className="input-label">Description</label><br/>
+                    <textarea className="inputclass" value={this.props.data}
+                        onChange={this.props.handleDescription} required></textarea><br/>
+                    <button type="Submit" className="submit-button">{this.props.editItem? 'Edit Record' : 'Add Record'}</button>
+                </form>
+            </div>
+        );
+        
+    }
+};
+export default ToDoForm;
